cf748d6ca0a39a2c9175adca2ac739c56cccf217
rkjeremy <randriakj@gmail.com>
2025-02-22 20:57:04 +0300

    initial release

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "the_trail_id" {
+  value       = aws_cloudtrail.the_trail.id
+  description = "The new trail's ID"
+}
 
+output "the_bucket_id" {
+  value       = aws_s3_bucket.the_bucket.id
+  description = "The new bucket's ID"
+}
+
+output "the_lambda_function_id" {
+  value       = aws_lambda_function.the_lambda_function.id
+  description = "The new lambda's ID"
+}
--- variables.tf
+++ variables.tf
@@ -1,27 +1,32 @@
 variable "aws_region" {
-  type     = string
-  nullable = false
+  type        = string
+  description = "The AWS region where resources will be provisioned."
+  nullable    = false
 }
 
 variable "enable_logging" {
-  type    = bool
-  default = true
+  type        = bool
+  description = "Whether to enable trail logging or not."
+  default     = true
 }
 
 variable "event_names" {
-  type    = list(string)
-  default = ["RunInstances", "TerminateInstances"]
+  type        = list(string)
+  description = "The list of event names you want to monitor."
+  default     = ["RunInstances", "TerminateInstances"]
 }
 
 variable "event_sources" {
-  type    = list(string)
-  default = ["ec2.amazonaws.com", "lambda.amazonaws.com", "s3.amazonaws.com", "kms.amazonaws.com"]
+  type        = list(string)
+  description = "The event sources you want to monitor."
+  default     = ["ec2.amazonaws.com", "lambda.amazonaws.com", "s3.amazonaws.com", "kms.amazonaws.com"]
 }
 
 variable "project_codename" {
-  type     = string
-  nullable = false
-  default  = "cocorico"
+  type        = string
+  description = "The name you want to give the project. This will be used as prefix for many created resources."
+  nullable    = false
+  default     = "cocorico"
 }
 
 variable "sns_topic_subscribers_email_address" {
